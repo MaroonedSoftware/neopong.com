b26ca5c0fa10ee2c25155948e6b26b39bd84a0b1
Robert Dean <knoxcoder@gmail.com>
2022-01-02 14:39:34 -0500

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Grid, Stack } from '@mui/material';
+import ProfileCard from './components/ProfileCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack>
+      <h1>Founders</h1>
+      <Grid container direction="row" justifyContent="space-evenly" alignItems="center">
+        <ProfileCard name='Charles Galyon' image='/cg.png' description='Design and Scripting' linkedin='https://www.linkedin.com/in/cgalyon/' />
+        <ProfileCard name='Eric Stamm' image='/es.png' description='Music and SFX' linkedin='https://www.linkedin.com/in/eric-stamm-a84229106/' />
+        <ProfileCard name='Robert Dean' image='/rd.png' description='Programming' linkedin='https://www.linkedin.com/in/knoxcoder/' />
+
+
+
+      </Grid>
+    </Stack>
   );
 }
 
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,32 @@
+import { Card, CardMedia, CardContent, Typography, CardActions, Button, IconButton } from "@mui/material";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+interface Props {
+    name: string;
+    image: string;
+    linkedin: string;
+    description: string;
+};
+
+const ProfileCard = ({ name, image, linkedin, description }: Props) => {
+    return (
+        <Card raised={true} sx={{ maxWidth: 345 }}>
+            <CardMedia component="img" height="140" image={image} alt={name} />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <IconButton href={linkedin}>
+                    <LinkedInIcon />
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default ProfileCard;
